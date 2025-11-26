be4c66a491cc712c8445b1a944435a4f922ba47e
Brok3nPix3l <rjbabala@yahoo.com>
2025-11-26 14:16:45 -0500

    Time: 68 ms (60.36%) | Memory: 122.7 MB (10.36%) - LeetSync

--- 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.java
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.java
@@ -7,34 +7,34 @@ class Solution {
     private static final int MOD = 1000000007;
     public int numberOfPaths(int[][] grid, int k) {
         final int n = grid.length, m = grid[0].length;
-        Map<Integer, Map<Integer, Map<Integer, Integer>>> dp = new HashMap<>();
+        int[][][] dp = new int[n][m][k];
         for (int r = 0; r < n; r++) {
             for (int c = 0; c < m; c++) {
                 // determine which of up and left neighbors have been visited (which means they have an entry in dp)
                 // if current cell value % k != 0, then add remainder to each key in adjacent neighbors' maps and merge them into current cell's map
                 int rem = grid[r][c] % k;
-                Map<Integer, Integer> numWaysToApproachByRem = new HashMap<>();
+                int[] numWaysToApproachByRem = new int[k];
                 if (r == 0 && c == 0) {
-                    numWaysToApproachByRem.put(rem, 1);
+                    numWaysToApproachByRem[rem] = 1;
                 }
                 if (r != 0) {
-                    Map<Integer, Integer> numWaysToApproachAboveNeighborByRem = dp.get(r - 1).get(c);
-                    for (Map.Entry<Integer, Integer> e : numWaysToApproachAboveNeighborByRem.entrySet()) {
-                        numWaysToApproachByRem.put((e.getKey() + rem) % k,
-                                (numWaysToApproachByRem.getOrDefault((e.getKey() + rem) % k, 0) + e.getValue()) % MOD);
+                    int[] numWaysToApproachAboveNeighborByRem = dp[r - 1][c];
+                    for (int i = 0; i < k; i++) {
+                        numWaysToApproachByRem[(i + rem) % k] += numWaysToApproachAboveNeighborByRem[i];
+                        numWaysToApproachByRem[(i + rem) % k] %= MOD;
                     }
                 }
                 if (c != 0) {
-                    Map<Integer, Integer> numWaysToApproachLeftNeighborByRem = dp.get(r).get(c - 1);
-                    for (Map.Entry<Integer, Integer> e : numWaysToApproachLeftNeighborByRem.entrySet()) {
-                        numWaysToApproachByRem.put((e.getKey() + rem) % k,
-                                (numWaysToApproachByRem.getOrDefault((e.getKey() + rem) % k, 0) + e.getValue()) % MOD);
+                    int[] numWaysToApproachLeftNeighborByRem = dp[r][c - 1];
+                    for (int i = 0; i < k; i++) {
+                        numWaysToApproachByRem[(i + rem) % k] += numWaysToApproachLeftNeighborByRem[i];
+                        numWaysToApproachByRem[(i + rem) % k] %= MOD;
                     }
                 }
-                dp.computeIfAbsent(r, _ -> new HashMap<>()).put(c, numWaysToApproachByRem);
+                dp[r][c] = numWaysToApproachByRem;
             }
         }
-        // System.out.println(dp);
-        return dp.get(n - 1).get(m - 1).getOrDefault(0, 0);
+        // System.out.println(Arrays.deepToString(dp));
+        return dp[n - 1][m - 1][0];
     }
 }
