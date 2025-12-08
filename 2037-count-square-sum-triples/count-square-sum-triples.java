445b462996c3ebe4f382884137be284de2bf739c
Brok3nPix3l <rjbabala@yahoo.com>
2025-12-07 19:52:36 -0500

    Time: 347 ms (5.01%) | Memory: 42.2 MB (26.30%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.java
+++ 2037-count-square-sum-triples/count-square-sum-triples.java
@@ -5,11 +5,10 @@ class Solution {
             for (int b = 1; b <= n; b++) {
                 int aAndB = a * a + b * b;
                 for (int c = 1; c <= n; c++) {
-                    int cSquared = c * c;
-                    if (aAndB < cSquared) {
+                    if (aAndB < c * c) {
                         continue;
                     }
-                    if (aAndB == cSquared) {
+                    if (aAndB == c * c) {
                         tripleCount++;
                     }
                 }
