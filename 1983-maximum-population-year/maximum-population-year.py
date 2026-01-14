9d48118e2663c0925db53c03e7d11b2715f4323a
Brok3nPix3l <rjbabala@yahoo.com>
2026-01-13 20:58:44 -0500

    Time: 1 ms (42.76%) | Memory: 19.4 MB (7.29%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.py
+++ 1983-maximum-population-year/maximum-population-year.py
@@ -6,31 +6,19 @@ class Solution:
 
     def maximumPopulation(self, logs: List[List[int]]) -> int:
         max_pop = 0
-        min_max_pop_year = self.END_YEAR
-        alive_people = [] # contains death years. population is lenth of this heap during given year
-        future_people = [] # contains people yet to be born. format is (birth year, death year)
-        for log in logs:
-            if log[0] <= self.START_YEAR:
-                heapq.heappush(alive_people, log[1])
-            else:
-                heapq.heappush(future_people, (log[0], log[1]))
+        min_max_pop_year = self.START_YEAR
         
-        # print(f'alive_people={alive_people} future_people={future_people}')
-        cur_pop = len(alive_people)
-        if cur_pop > max_pop:
-            max_pop = cur_pop
-            min_max_pop_year = self.START_YEAR
+        population_during_year = [0 for y in range(self.END_YEAR - self.START_YEAR + 1)]
+        for log in logs:
+            population_during_year[log[0] - self.START_YEAR] += 1
+            population_during_year[log[1] - self.START_YEAR] -= 1
+        # print(population_during_year)
 
-        for y in range(self.START_YEAR + 1, self.END_YEAR):
-            while alive_people and alive_people[0] <= y:
-                heapq.heappop(alive_people)
-            
-            while future_people and future_people[0][0] <= y:
-                heapq.heappush(alive_people, heapq.heappop(future_people)[1])
-
-            cur_pop = len(alive_people)
+        cur_pop = 0
+        for year_offset, population in enumerate(population_during_year):
+            cur_pop += population
             if cur_pop > max_pop:
                 max_pop = cur_pop
-                min_max_pop_year = y
+                min_max_pop_year = year_offset + self.START_YEAR
         
         return min_max_pop_year
