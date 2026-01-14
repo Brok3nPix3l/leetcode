02742769ccd90105a761a893c1479bcb10c89ec0
Brok3nPix3l <rjbabala@yahoo.com>
2026-01-13 20:53:44 -0500

    Time: 1 ms (42.76%) | Memory: 19.5 MB (7.29%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.py
+++ 1983-maximum-population-year/maximum-population-year.py
@@ -1,3 +1,5 @@
+import heapq
+
 class Solution:
     START_YEAR = 1950
     END_YEAR = 2050
@@ -5,18 +7,30 @@ class Solution:
     def maximumPopulation(self, logs: List[List[int]]) -> int:
         max_pop = 0
         min_max_pop_year = self.END_YEAR
-        for y in range(self.START_YEAR, self.END_YEAR):
-            cur_pop = self.population(y, logs)
+        alive_people = [] # contains death years. population is lenth of this heap during given year
+        future_people = [] # contains people yet to be born. format is (birth year, death year)
+        for log in logs:
+            if log[0] <= self.START_YEAR:
+                heapq.heappush(alive_people, log[1])
+            else:
+                heapq.heappush(future_people, (log[0], log[1]))
+        
+        # print(f'alive_people={alive_people} future_people={future_people}')
+        cur_pop = len(alive_people)
+        if cur_pop > max_pop:
+            max_pop = cur_pop
+            min_max_pop_year = self.START_YEAR
+
+        for y in range(self.START_YEAR + 1, self.END_YEAR):
+            while alive_people and alive_people[0] <= y:
+                heapq.heappop(alive_people)
+            
+            while future_people and future_people[0][0] <= y:
+                heapq.heappush(alive_people, heapq.heappop(future_people)[1])
+
+            cur_pop = len(alive_people)
             if cur_pop > max_pop:
                 max_pop = cur_pop
                 min_max_pop_year = y
         
         return min_max_pop_year
-
-    def population(self, year: int, logs: List[List[int]]) -> int:
-        pop = 0
-        for log in logs:
-            if log[0] <= year and log[1] > year:
-                pop += 1
-
-        return pop
