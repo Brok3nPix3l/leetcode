d92eaf1cdda5cc8cb79aad6003841a16dbb7f46b
Brok3nPix3l <rjbabala@yahoo.com>
2026-01-15 19:28:09 -0500

    Time: 3 ms (38.44%) | Memory: 19.1 MB (16.92%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
@@ -1,27 +1,3 @@
 class Solution:
     def countGoodSubstrings(self, s: str) -> int:
-        count = 0
-        
-        char_count = {}
-        l = 0
-        for r in range(len(s)):
-            if s[r] not in char_count:
-                char_count[s[r]] = 0
-            char_count[s[r]] += 1
-            if r - l == 2:
-                if self.is_good(char_count):
-                    count += 1
-                char_count[s[l]] -= 1
-                if char_count[s[l]] == 0:
-                    del char_count[s[l]]
-                l += 1
-            # print(char_count)
-        
-        return count
-
-    def is_good(self, char_count: dict[chr, int]) -> bool:
-        for c in char_count:
-            if char_count[c] > 1:
-                return False
-        
-        return True
+        return sum((1 if (len(set(s[i:i+3])) == 3) else 0) for i in range(len(s) - 2))
