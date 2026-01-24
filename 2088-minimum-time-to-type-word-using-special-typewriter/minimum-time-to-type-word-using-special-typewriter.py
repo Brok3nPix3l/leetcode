077f183ec1005008ca4d4cc0a6e0a675cbef8ab5
Brok3nPix3l <rjbabala@yahoo.com>
2026-01-24 10:51:45 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (20.08%) - LeetSync

--- 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.py
+++ 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.py
@@ -6,12 +6,12 @@ class Solution:
         time = 0
         for c in word:
             clockwise_time = (ord(c) - ord(cur)) % self.WHEEL_SPACES
-            counterclockwise_time = (ord(cur) - ord(c)) % self.WHEEL_SPACES
+            # counterclockwise_time = (ord(cur) - ord(c)) % self.WHEEL_SPACES
             # print(f'{cur}->{c} clockwise_time={clockwise_time} counterclockwise_time={counterclockwise_time}')
-            if clockwise_time < counterclockwise_time:
+            if clockwise_time < self.WHEEL_SPACES / 2:
                 time_taken = clockwise_time
             else:
-                time_taken = counterclockwise_time
+                time_taken = self.WHEEL_SPACES - clockwise_time
             
             time += time_taken + 1
             cur = c
