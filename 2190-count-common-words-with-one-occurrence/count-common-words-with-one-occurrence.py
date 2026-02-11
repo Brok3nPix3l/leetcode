79b83cd9c9c9b5b0a594b95c3d03ab15cdf432de
Brok3nPix3l <rjbabala@yahoo.com>
2026-02-11 15:43:07 -0500

    Time: 3 ms (66.43%) | Memory: 19.8 MB (6.50%) - LeetSync

--- 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.py
+++ 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.py
@@ -3,7 +3,12 @@ class Solution:
         c1 = Counter(words1)
         c2 = Counter(words2)
         ans = 0
-        for word, freq in c1.items():
-            if freq == 1 and c2[word] == 1:
-                ans += 1
+        if len(c1) <= len(c2):
+            for word, freq in c1.items():
+                if freq == 1 and c2[word] == 1:
+                    ans += 1
+        else:
+            for word, freq in c2.items():
+                if freq == 1 and c1[word] == 1:
+                    ans += 1
         return ans
