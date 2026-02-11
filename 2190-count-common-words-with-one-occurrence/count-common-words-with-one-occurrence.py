c6a3e8b120dbf9b23678e48c11803c56d59f5627
Brok3nPix3l <rjbabala@yahoo.com>
2026-02-11 15:42:35 -0500

    Time: 0 ms (100.00%) | Memory: 19.6 MB (60.74%) - LeetSync

--- 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.py
+++ 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.py
@@ -2,9 +2,8 @@ class Solution:
     def countWords(self, words1: List[str], words2: List[str]) -> int:
         c1 = Counter(words1)
         c2 = Counter(words2)
-        shorter = c1 if len(c1) <= len(c2) else c2
         ans = 0
-        for word in shorter:
-            if c1[word] == 1 and c2[word] == 1:
+        for word, freq in c1.items():
+            if freq == 1 and c2[word] == 1:
                 ans += 1
         return ans
