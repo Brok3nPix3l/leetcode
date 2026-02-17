98eb8f3494bf1ab3c3640743898c45334cabea8b
Brok3nPix3l <rjbabala@yahoo.com>
2026-02-16 22:22:29 -0500

    Time: 52 ms (25.03%) | Memory: 21.2 MB (7.15%) - LeetSync

--- 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.py
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.py
@@ -1,11 +1,18 @@
 class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
+        n = len(matrix)
         rows = [set() for _ in matrix]
         cols = [set() for _ in matrix[0]]
         for r, row in enumerate(matrix):
             for c, cell in enumerate(row):
-                if cell in rows[r] or cell in cols[c]:
-                    return False
+                # if cell in rows[r] or cell in cols[c]:
+                #     return False
                 rows[r].add(cell)
                 cols[c].add(cell)
+        for row in rows:
+            if len(row) < n:
+                return False
+        for col in cols:
+            if len(col) < n:
+                return False
         return True
