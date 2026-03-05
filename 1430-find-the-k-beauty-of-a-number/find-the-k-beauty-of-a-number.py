3132644767a12b44e38a05724421d308ddc985a5
Brok3nPix3l <rjbabala@yahoo.com>
2026-03-04 22:45:36 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (19.73%) - LeetSync

--- 1430-find-the-k-beauty-of-a-number/find-the-k-beauty-of-a-number.py
+++ 1430-find-the-k-beauty-of-a-number/find-the-k-beauty-of-a-number.py
@@ -1,10 +1,3 @@
 class Solution:
     def divisorSubstrings(self, num: int, k: int) -> int:
-        ans = 0
-        for i in range(len(str(num)) + 1 - k):
-            cur = int(num / 10 ** i % 10 ** k)
-            if cur == 0:
-                continue
-            if num % cur == 0:
-                ans += 1
-        return ans
+        return sum((1 if int(num / 10 ** i % 10 ** k) != 0 and num % int(num / 10 ** i % 10 ** k) == 0 else 0) for i in range(len(str(num)) + 1 - k))
