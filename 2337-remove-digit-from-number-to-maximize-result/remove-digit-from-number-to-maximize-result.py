e6bcb53fd26cc0105ad20adaf6d92f331f2f0a9b
Brok3nPix3l <rjbabala@yahoo.com>
2026-03-04 22:32:20 -0500

    Time: 0 ms (100.00%) | Memory: 19.2 MB (87.28%) - LeetSync

--- 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.py
+++ 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.py
@@ -3,7 +3,5 @@ class Solution:
         max_num = 0
         for i, d in enumerate(number):
             if d == digit:
-                substring_with_digit_removed = number[0:i] + number[i+1:]
-                substring_int = int(substring_with_digit_removed)
-                max_num = max(max_num, substring_int)
+                max_num = max(max_num, int(number[0:i] + number[i+1:]))
         return str(max_num)
