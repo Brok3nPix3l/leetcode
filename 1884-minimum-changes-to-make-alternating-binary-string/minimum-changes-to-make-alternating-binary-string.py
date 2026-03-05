6b40135e0de445ccca9a75d3da47d14c88e394ee
Brok3nPix3l <rjbabala@yahoo.com>
2026-03-04 20:22:50 -0500

    Time: 15 ms (18.13%) | Memory: 19.3 MB (83.47%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.py
@@ -1,8 +1,9 @@
 class Solution:
     def minOperations(self, s: str) -> int:
+        arr = [int(d) for d in s]
         ops = [0, 0]
-        for i, d in enumerate(s):
-            if int(d) == i % 2:
+        for i, d in enumerate(arr):
+            if d == i % 2:
                 ops[1] += 1
             else:
                 ops[0] += 1
