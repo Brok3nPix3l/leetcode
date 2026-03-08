8f501da56acd52cd0036e6e36b05f0652d2db8db
Brok3nPix3l <rjbabala@yahoo.com>
2026-03-08 16:48:25 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (73.18%) - LeetSync

--- 2391-strong-password-checker-ii/strong-password-checker-ii.py
+++ 2391-strong-password-checker-ii/strong-password-checker-ii.py
@@ -3,34 +3,18 @@ class Solution:
         if len(password) < 8:
             return False
         contains_lowercase_letter = False
-        for c in password:
+        contains_uppercase_letter = False
+        contains_digit = False
+        contains_special_character = False
+        for i, c in enumerate(password):
+            if i < len(password) - 1 and password[i+1] == c:
+                return False
             if c.islower():
                 contains_lowercase_letter = True
-                break
-        if not contains_lowercase_letter:
-            return False
-        contains_uppercase_letter = False
-        for c in password:
-            if c.isupper():
+            elif c.isupper():
                 contains_uppercase_letter = True
-                break
-        if not contains_uppercase_letter:
-            return False
-        contains_digit = False
-        for c in password:
-            if c.isdigit():
+            elif c.isdigit():
                 contains_digit = True
-                break
-        if not contains_digit:
-            return False
-        contains_special_character = False
-        for c in password:
-            if c in "!@#$%^&*()-+":
+            elif c in "!@#$%^&*()-+":
                 contains_special_character = True
-                break
-        if not contains_special_character:
-            return False
-        for i, c in enumerate(password[:-1]):
-            if password[i+1] == c:
-                return False
-        return True
+        return contains_lowercase_letter and contains_uppercase_letter and contains_digit and contains_special_character
