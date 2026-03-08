f4f3a4ef2fff76627af61846da915a2f259fbc9c
Brok3nPix3l <rjbabala@yahoo.com>
2026-03-08 00:45:41 -0500

    Time: 10 ms (2.75%) | Memory: 19.4 MB (61.71%) - LeetSync

--- 2386-min-max-game/min-max-game.py
+++ 2386-min-max-game/min-max-game.py
@@ -1,11 +1,9 @@
 class Solution:
     def minMaxGame(self, nums: List[int]) -> int:
-        while len(nums) > 1:
-            new_nums = []
+        for _ in range(int(log2(len(nums)))):
             for i in range(len(nums) // 2):
                 if i % 2 == 0:
-                    new_nums.append(min(nums[2 * i], nums[2 * i + 1]))
+                    nums[i] = min(nums[2 * i], nums[2 * i + 1])
                 else:
-                    new_nums.append(max(nums[2 * i], nums[2 * i + 1]))
-            nums = new_nums
+                    nums[i] = max(nums[2 * i], nums[2 * i + 1])
         return nums[0]
