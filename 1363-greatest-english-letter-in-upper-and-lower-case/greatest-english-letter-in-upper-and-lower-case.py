70bb911705c47277ac8f2bd9220b5a50e6e43ffb
Brok3nPix3l <rjbabala@yahoo.com>
2026-03-08 23:53:13 -0400

    Time: 4 ms (44.47%) | Memory: 19.1 MB (98.19%) - LeetSync

--- 1363-greatest-english-letter-in-upper-and-lower-case/greatest-english-letter-in-upper-and-lower-case.py
+++ 1363-greatest-english-letter-in-upper-and-lower-case/greatest-english-letter-in-upper-and-lower-case.py
@@ -1,7 +1,9 @@
+import string
+
 class Solution:
     def greatestLetter(self, s: str) -> str:
         s = set(s)
-        for l in "ZYXWVUTSRQPONMLKJIHGFEDCBA":
+        for l in string.ascii_uppercase[::-1]:
             if l in s and l.lower() in s:
                 return l
         return ""
