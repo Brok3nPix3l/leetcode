b47e90d6b1264a55e4e08a41977224129f83023b
Brok3nPix3l <rjbabala@yahoo.com>
2026-03-13 22:10:33 -0400

    Time: 5 ms (67.71%) | Memory: 19.2 MB (97.88%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -1,23 +1,27 @@
 class Solution:
     def __init__(self):
         self.happyStrings = []
+        self.kthHappyString = None
 
     def getHappyString(self, n: int, k: int) -> str:
-        self.appendHappyStrings(n, '')
-        # print(self.happyStrings)
-        if len(self.happyStrings) >= k:
-            return self.happyStrings[k-1]
-        return ''
+        return self.appendHappyStrings(n, k, '') or ''
     
-    def appendHappyStrings(self, n: int, s: str) -> None:
+    def appendHappyStrings(self, n: int, k: int, s: str) -> Union[str, None]:
         if len(s) == n:
-            # print(f's={s}')
             self.happyStrings.append(s)
-            return
+            if len(self.happyStrings) == k:
+                self.kthHappyString = s
+            return s
         
         if len(s) == 0 or s[-1] != 'a':
-            self.appendHappyStrings(n, s + 'a')
+            self.appendHappyStrings(n, k, s + 'a')
+        if self.kthHappyString:
+            return self.kthHappyString
         if len(s) == 0 or s[-1] != 'b':
-            self.appendHappyStrings(n, s + 'b')
+            self.appendHappyStrings(n, k, s + 'b')
+        if self.kthHappyString:
+            return self.kthHappyString
         if len(s) == 0 or s[-1] != 'c':
-            self.appendHappyStrings(n, s + 'c')
+            self.appendHappyStrings(n, k, s + 'c')
+        if self.kthHappyString:
+            return self.kthHappyString
