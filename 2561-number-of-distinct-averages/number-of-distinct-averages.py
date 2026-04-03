f7b9d07dfb9324c214dc2b6f82f9339f20959384
Brok3nPix3l <rjbabala@yahoo.com>
2026-04-03 09:57:08 -0400

    Time: 4 ms (2.59%) | Memory: 19.2 MB (74.70%) - LeetSync

--- 2561-number-of-distinct-averages/number-of-distinct-averages.py
+++ 2561-number-of-distinct-averages/number-of-distinct-averages.py
@@ -1,7 +1,13 @@
 class Solution:
     def distinctAverages(self, nums: List[int]) -> int:
         nums.sort()
-        avgs = set()
-        while len(nums) > 0:
-            avgs.add((nums.pop() + nums.pop(0)) / 2)
-        return len(avgs)
+        sums = set()
+        l = 0
+        r = len(nums) - 1
+        while l < r:
+            # store the sum instead of the average
+            # since we're always taking the average of 2 numbers
+            sums.add(nums[r] + nums[l])
+            l += 1
+            r -= 1
+        return len(sums)
