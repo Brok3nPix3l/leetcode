3f6d3e4c383d2e2373b0875a019301fc6538c732
Brok3nPix3l <rjbabala@yahoo.com>
2026-04-14 21:27:23 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (45.77%) - LeetSync

---
+++ 2619-categorize-box-according-to-criteria/categorize-box-according-to-criteria.py
@@ -0,0 +1,16 @@
+class Solution:
+    BULKY_DIMENSION_SIZE = 10 ** 4
+    BULKY_VOLUME = 10 ** 9
+
+    def categorizeBox(self, length: int, width: int, height: int, mass: int) -> str:
+        volume = length * width * height
+        isBulky = length >= self.BULKY_DIMENSION_SIZE or width >= self.BULKY_DIMENSION_SIZE or height >= self.BULKY_DIMENSION_SIZE or volume >= self.BULKY_VOLUME
+        isHeavy = mass >= 100
+        if isBulky and isHeavy:
+            return 'Both'
+        elif isBulky:
+            return 'Bulky'
+        elif isHeavy:
+            return 'Heavy'
+        else:
+            return 'Neither'
