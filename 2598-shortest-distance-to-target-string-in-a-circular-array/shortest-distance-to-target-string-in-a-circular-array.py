8c42121754c40a878f68fd76705b85a1eec55b41
Brok3nPix3l <rjbabala@yahoo.com>
2026-04-14 21:21:38 -0400

    Time: 0 ms (100.00%) | Memory: 19.5 MB (4.30%) - LeetSync

--- 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.py
+++ 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.py
@@ -1,9 +1,13 @@
 class Solution:
     def closestTarget(self, words: List[str], target: str, startIndex: int) -> int:
+        """
+        use two pointers moving in opposite directions to find the target
+        each pointer only needs to cover half of the array, since there's no overlap after the initial state
+        if we don't manage to find the target during this, then it's not present at all
+        """
         n = len(words)
         l, r = startIndex, startIndex
-        lte, gte = False, False
-        for steps in range(ceil(len(words) / 2) + 1):
+        for steps in range(floor(len(words) / 2) + 1):
             # print(f'steps={steps} words[{l}]={words[l]} words[{r}]={words[r]}')
             if words[l] == target or words[r] == target:
                 return steps
