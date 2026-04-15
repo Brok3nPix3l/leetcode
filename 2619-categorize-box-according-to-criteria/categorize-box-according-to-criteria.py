7da4390910fd2608936c5d3df871eca17694b133
Brok3nPix3l <rjbabala@yahoo.com>
2026-04-14 21:30:01 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (45.77%) - LeetSync

--- 2619-categorize-box-according-to-criteria/categorize-box-according-to-criteria.py
+++ 2619-categorize-box-according-to-criteria/categorize-box-according-to-criteria.py
@@ -4,7 +4,8 @@ class Solution:
 
     def categorizeBox(self, length: int, width: int, height: int, mass: int) -> str:
         volume = length * width * height
-        isBulky = length >= self.BULKY_DIMENSION_SIZE or width >= self.BULKY_DIMENSION_SIZE or height >= self.BULKY_DIMENSION_SIZE or volume >= self.BULKY_VOLUME
+        isBulky = any(dimension >= self.BULKY_DIMENSION_SIZE for dimension in (length, width, height)) \
+                  or volume >= self.BULKY_VOLUME
         isHeavy = mass >= 100
         if isBulky and isHeavy:
             return 'Both'
