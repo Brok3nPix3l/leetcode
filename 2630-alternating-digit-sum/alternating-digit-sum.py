479a7d20b9d342ad61ab1476cac31ad7d289932b
Brok3nPix3l <rjbabala@yahoo.com>
2026-04-18 15:02:47 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (58.99%) - LeetSync

--- 2630-alternating-digit-sum/alternating-digit-sum.py
+++ 2630-alternating-digit-sum/alternating-digit-sum.py
@@ -1,4 +1,6 @@
 class Solution:
     def alternateDigitSum(self, n: int) -> int:
         s = str(n)
-        return sum(int(d) for d in s[::2]) - sum(int(d) for d in s[1::2])
+        p = sum(int(d) for d in s[::2])
+        n = sum(int(d) for d in s[1::2])
+        return p - n
