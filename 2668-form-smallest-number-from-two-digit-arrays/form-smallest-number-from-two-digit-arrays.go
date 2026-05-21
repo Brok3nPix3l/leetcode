52f29751994e6710924f1d333254031e19b8c377
Brok3nPix3l <rjbabala@yahoo.com>
2026-05-20 20:40:29 -0400

    Time: 0 ms (100.00%) | Memory: 4 MB (100.00%) - LeetSync

--- 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.go
+++ 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.go
@@ -1,22 +1,23 @@
 const maxNum = 9
 
 func minNumber(nums1 []int, nums2 []int) int {
-    nums1Set := make(map[int]struct{})
+    set1 := make(map[int]struct{}, len(nums1))
     for _, v := range nums1 {
-        nums1Set[v] = struct{}{}
+        set1[v] = struct{}{}
     }
-    minValInBothArrays := maxNum + 1
+    minBoth := maxNum + 1
     for _, v := range nums2 {
-        if _, ok := nums1Set[v]; ok {
-            minValInBothArrays = min(v, minValInBothArrays)
+        if _, ok := set1[v]; ok && v < minBoth {
+            minBoth = v
         }
     }
-    if minValInBothArrays <= maxNum {
-        return minValInBothArrays
+    if minBoth <= maxNum {
+        return minBoth
     }
-    nums1Min := slices.Min(nums1)
-    nums2Min := slices.Min(nums2)
-    // fmt.Println("nums1 min val:", nums1Min, "nums2 min val:", nums2Min)
-    ans, _ := strconv.Atoi(strconv.Itoa(min(nums1Min, nums2Min)) + strconv.Itoa(max(nums1Min, nums2Min)))
+    a, b := slices.Min(nums1), slices.Min(nums2)
+    if a > b {
+        a, b = b, a
+    }
+    ans, _ := strconv.Atoi(fmt.Sprintf("%d%d", a, b))
     return ans
 }
