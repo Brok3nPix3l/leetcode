ec8bf4af42d080ff6539fb95bb50f01a7280deaa
Brok3nPix3l <rjbabala@yahoo.com>
2026-05-20 20:34:38 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (61.33%) - LeetSync

--- 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.py
+++ 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.py
@@ -1,8 +1,7 @@
 class Solution:
     def minNumber(self, nums1: List[int], nums2: List[int]) -> int:
         intersect = set(nums1) & set(nums2)
-        if len(intersect) > 0:
+        if intersect:
             return min(intersect)
-        mn1 = min(nums1)
-        mn2 = min(nums2)
-        return int(str(min(mn1, mn2)) + str(max(mn1, mn2)))
+        a, b = sorted((min(nums1), min(nums2)))
+        return int(f'{a}{b}')
