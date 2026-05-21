ea2ac14fbb46208d1ac5ee86a8b1bed32d66561c
Brok3nPix3l <rjbabala@yahoo.com>
2026-05-21 17:40:44 -0400

    Time: 0 ms (100.00%) | Memory: 7.5 MB (100.00%) - LeetSync

--- 2675-find-the-width-of-columns-of-a-grid/find-the-width-of-columns-of-a-grid.go
+++ 2675-find-the-width-of-columns-of-a-grid/find-the-width-of-columns-of-a-grid.go
@@ -3,13 +3,47 @@ func findColumnWidth(grid [][]int) []int {
     for _, nums := range grid {
         for j, num := range nums {
             // fmt.Println("j:", j, "num:", num)
-            curLen := len(strconv.Itoa(num))
             if j >= len(widths) {
-                widths = append(widths, curLen)
+                widths = append(widths, intLen(num))
             } else {
-                widths[j] = max(widths[j], curLen)
+                widths[j] = max(widths[j], intLen(num))
             }
         }
     }
     return widths
+}
+
+func intLen(i int) int {
+    if i == 0 {
+        return 1
+    }
+    length := 0
+    if i < 0 {
+        length = 1
+        i = -i
+    }
+    switch {
+        case i < 10:
+            return length + 1
+        case i < 100:
+            return length + 2
+        case i < 1000:
+            return length + 3
+        case i < 10000:
+            return length + 4
+        case i < 100000:
+            return length + 5
+        case i < 100000:
+            return length + 5
+        case i < 1000000:
+            return length + 6
+        case i < 10000000:
+            return length + 7
+        case i < 100000000:
+            return length + 8
+        case i < 1000000000:
+            return length + 9
+        default:
+            return length + 10
+    }
 }
