a7c296cbda2e9623c43b02a897b4f5b8b531d8a2
Brok3nPix3l <rjbabala@yahoo.com>
2026-06-13 12:44:32 -0400

    Time: 15 ms (15.97%) | Memory: 19.3 MB (72.25%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.py
+++ 4216-weighted-word-mapping/weighted-word-mapping.py
@@ -1,7 +1,3 @@
 class Solution:
-    def __init__(self):
-        self.a_ord = ord('a')
-        self.z_ord = ord('z')
-
     def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
-        return "".join([chr(self.z_ord - weight % 26) for weight in [sum(weights[ord(c) - self.a_ord] for c in word) for word in words]])
+        return "".join([chr(122 - weight % 26) for weight in [sum(weights[ord(c) - 97] for c in word) for word in words]])
