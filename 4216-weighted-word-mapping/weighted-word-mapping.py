c761f4852d36cff15fd163d8ecd13ae291746840
Brok3nPix3l <rjbabala@yahoo.com>
2026-06-13 12:44:50 -0400

    Time: 9 ms (56.74%) | Memory: 19.2 MB (72.25%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.py
+++ 4216-weighted-word-mapping/weighted-word-mapping.py
@@ -1,3 +1,3 @@
 class Solution:
     def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
-        return "".join([chr(122 - weight % 26) for weight in [sum(weights[ord(c) - 97] for c in word) for word in words]])
+        return "".join([chr(ord('z') - weight % 26) for weight in [sum(weights[ord(c) - ord('a')] for c in word) for word in words]])
