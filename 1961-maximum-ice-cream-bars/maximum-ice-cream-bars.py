4aa509a25ca7c27a7db836bcb7d244319ce018e1
Brok3nPix3l <rjbabala@yahoo.com>
2026-06-20 21:09:25 -0400

    Time: 95 ms (21.86%) | Memory: 34.1 MB (8.84%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
@@ -1,17 +1,17 @@
+from collections import Counter
+
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
-        mx = max(costs)
-        freq = [0] * (mx + 1)
-        for cost in costs:
-            freq[cost] += 1
+        c = Counter(costs)
         ice_cream_count = 0
-        for cost, f in enumerate(freq):
-            if f == 0:
+        for cost in sorted(c):
+            freq = c[cost]
+            if freq == 0:
                 continue
-            total_cost = cost * f
+            total_cost = cost * freq
             if coins >= total_cost:
                 coins -= total_cost
-                ice_cream_count += f
+                ice_cream_count += freq
             else:
                 ice_cream_count += coins // cost
                 break
