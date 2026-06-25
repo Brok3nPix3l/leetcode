9635c667302db8737d252fcdecd860a009010319
Brok3nPix3l <rjbabala@yahoo.com>
2026-06-25 14:14:04 -0400

    Time: 276 ms (18.62%) | Memory: 19.2 MB (79.66%) - LeetSync

--- 2831-number-of-beautiful-pairs/number-of-beautiful-pairs.py
+++ 2831-number-of-beautiful-pairs/number-of-beautiful-pairs.py
@@ -2,7 +2,4 @@ import math
 
 class Solution:
     def countBeautifulPairs(self, nums: List[int]) -> int:
-        return sum(1 if self.is_beautiful_pair(int(str(nums[i])[0]), int(str(nums[j])[-1])) else 0 for i in range(len(nums)) for j in range(i + 1, len(nums)))
-    
-    def is_beautiful_pair(self, x: int, y: int) -> bool:
-        return math.gcd(x, y) == 1
+        return sum(1 if math.gcd(int(str(nums[i])[0]), int(str(nums[j])[-1])) == 1 else 0 for i in range(len(nums)) for j in range(i + 1, len(nums)))
