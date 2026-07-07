563a7946f98ce38805c5bef8b266bc11d8d1843b
Brok3nPix3l <rjbabala@yahoo.com>
2026-07-07 19:05:06 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (14.25%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.py
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.py
@@ -2,9 +2,11 @@ class Solution:
     def sumAndMultiply(self, n: int) -> int:
         x = "".join(d for d in str(n) if d != '0')
         if x == '':
-            # x = '0'
+            """
+            - x == '0'
+            - s = 0
+            - int(x) * s == 0 * 0 == 0
+            """
             return 0
-        # print(x)
         s = sum(int(d) for d in x)
-        # print(s)
         return int(x) * s
