1fccd19311846b40348fd57f293e052eece0e290
Brok3nPix3l <rjbabala@yahoo.com>
2026-07-10 14:19:08 -0400

    Time: 3 ms (84.07%) | Memory: 19.4 MB (38.05%) - LeetSync

--- 2881-split-strings-by-separator/split-strings-by-separator.py
+++ 2881-split-strings-by-separator/split-strings-by-separator.py
@@ -1,3 +1,3 @@
 class Solution:
     def splitWordsBySeparator(self, words: List[str], separator: str) -> List[str]:
-        return list(filter(lambda x: bool(x), (s for word in words for s in word.split(separator))))
+        return [s for word in words for s in word.split(separator) if s]
