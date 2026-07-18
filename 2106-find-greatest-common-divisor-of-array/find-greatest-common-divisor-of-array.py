1f6589194da8feb4e0a0179c5371fb784b2ae373
Brok3nPix3l <rjbabala@yahoo.com>
2026-07-17 21:05:02 -0400

    Time: 0 ms (100.00%) | Memory: 19.5 MB (11.34%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.py
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.py
@@ -1,23 +1,18 @@
+import math
+
 class Solution:
     def findGCD(self, nums: List[int]) -> int:
         smallest, largest = self.smallest_and_largest(nums)
         
-        return self.gcd(smallest, largest)
+        return math.gcd(smallest, largest)
 
     def smallest_and_largest(self, nums: list[nums]) -> tuple[int, int]:
         smallest = None
         largest = None
         for n in nums:
-            if not smallest or n < smallest:
+            if smallest == None or n < smallest:
                 smallest = n
-            if not largest or n > largest:
+            if largest == None or n > largest:
                 largest = n
         
         return (smallest, largest)
-
-    def gcd(self, n1, n2):
-        while n1 != 0:
-            temp = n1
-            n1 = n2 % n1
-            n2 = temp
-        return n2
