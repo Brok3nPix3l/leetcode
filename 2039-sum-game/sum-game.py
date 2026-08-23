875302e18cd022d8e989c207605c962a27e8c89b
Brok3nPix3l <rjbabala@yahoo.com>
2026-08-23 18:36:16 -0400

    Time: 63 ms (32.35%) | Memory: 20 MB (27.65%) - LeetSync

--- 2039-sum-game/sum-game.py
+++ 2039-sum-game/sum-game.py
@@ -1,17 +1,20 @@
 class Solution:
+    class SumGameString:
+        def __init__(self, s: str):
+            n = len(s)
+            
+            self.firstHalfQuestionMarkCount = s.count('?', 0, n // 2)
+            self.secondHalfQuestionMarkCount = s.count('?', n // 2, n)
+
+            self.firstHalfDigitSum = sum(int(s[i]) if s[i] != '?' else 0 for i in range(n // 2))
+            self.secondHalfDigitSum = sum(int(s[i]) if s[i] != '?' else 0 for i in range(n // 2, n))
+    
     def sumGame(self, num: str) -> bool:
-        n = len(num)
-        
-        q1 = num.count('?', 0, n // 2)
-        q2 = num.count('?', n // 2, n)
-
-        s1 = sum(int(num[i]) if num[i] != '?' else 0 for i in range(n // 2))
-        s2 = sum(int(num[i]) if num[i] != '?' else 0 for i in range(n // 2, n))
-
+        s = self.SumGameString(num)
         # if the sum of the digits in one half of the string, s1, is less than the number of question marks in excess in the second half of the string, q2 - q1, times 9, then Alice will win
-        if ((q1 - q2) // 2) * 9 > s2 - s1:
+        if ((s.firstHalfQuestionMarkCount - s.secondHalfQuestionMarkCount) // 2) * 9 > s.secondHalfDigitSum - s.firstHalfDigitSum:
             return True
-        if ((q2 - q1) // 2) * 9 > s1 - s2:
+        if ((s.secondHalfQuestionMarkCount - s.firstHalfQuestionMarkCount) // 2) * 9 > s.firstHalfDigitSum - s.secondHalfDigitSum:
             return True
         # if there is an odd number of question marks, Bob will win
-        return abs(q1 - q2) % 2 == 1
+        return abs(s.firstHalfQuestionMarkCount - s.secondHalfQuestionMarkCount) % 2 == 1
