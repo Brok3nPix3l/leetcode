4cd49028e5cfb21fe6848512c1c3aeded6602b95
Brok3nPix3l <rjbabala@yahoo.com>
2026-08-23 19:18:19 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (59.07%) - LeetSync

--- 3324-split-the-array/split-the-array.py
+++ 3324-split-the-array/split-the-array.py
@@ -1,14 +1,13 @@
-from collections import defaultdict
-
 class Solution:
     def isPossibleToSplit(self, nums: List[int]) -> bool:
         # rephrasing the question; are the following statements true:
         # - all elements appear exactly one or two times
         # - an number of elements appear exactly once (this is guaranteed if the above is true)
-        f = defaultdict(int)
+        f = {}
         for num in nums:
-            f[num] += 1
-            if f[num] == 3:
+            numFreq = f.get(num, 0) + 1
+            if numFreq == 3:
                 return False
+            f[num] = numFreq
         
         return True
