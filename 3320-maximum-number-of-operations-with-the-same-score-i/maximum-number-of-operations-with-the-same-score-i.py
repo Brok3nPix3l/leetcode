1b8ec12289d6a2be94abfdbd887208fa597fa6d3
Brok3nPix3l <rjbabala@yahoo.com>
2026-08-23 18:51:30 -0400

    Time: 3 ms (7.82%) | Memory: 19.2 MB (54.75%) - LeetSync

--- 3320-maximum-number-of-operations-with-the-same-score-i/maximum-number-of-operations-with-the-same-score-i.py
+++ 3320-maximum-number-of-operations-with-the-same-score-i/maximum-number-of-operations-with-the-same-score-i.py
@@ -3,11 +3,7 @@ class Solution:
         targetScore = nums[0] + nums[1]
         maxOps = 1
 
-        if len(nums) % 2 == 0:
-            upperBound = len(nums)
-        else:
-            upperBound = len(nums) - 1
-        for i in range(2, upperBound, 2):
+        for i in range(2, len(nums) & ~1, 2):
             curScore = nums[i] + nums[i + 1]
             if curScore != targetScore:
                 break
