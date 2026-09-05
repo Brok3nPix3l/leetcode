0004a34397889406914e19d35ffe88e288b15c27
Brok3nPix3l <rjbabala@yahoo.com>
2026-09-04 20:07:25 -0400

    Time: 18 ms (35.65%) | Memory: 19.3 MB (67.13%) - LeetSync

--- 3543-count-substrings-that-satisfy-k-constraint-i/count-substrings-that-satisfy-k-constraint-i.py
+++ 3543-count-substrings-that-satisfy-k-constraint-i/count-substrings-that-satisfy-k-constraint-i.py
@@ -1,5 +1,21 @@
 class Solution:
     def countKConstraintSubstrings(self, s: str, k: int) -> int:
+        """
+        sliding window
+        
+        accumulate the valid substrings one window at a time
+
+        for a given window spanning s[l..r], there are r - l + 1 unique valid substrings that have not already been accounted for
+
+        e.g.
+        10101
+        ^       0 - 0 + 1 = 1 [1]           1 total
+        ^^      1 - 0 + 1 = 2 [0, 10]       3 total
+        ^ ^     2 - 0 + 1 = 3 [1, 01, 101]  6 total
+         ^ ^    3 - 1 + 1 = 3 [0, 10, 010]  9 total
+          ^ ^   4 - 2 + 1 = 3 [1, 01, 101]  12 total
+        """
+
         substringCount = 0
         
         bitsInWindow = [0] * 2
@@ -10,9 +26,6 @@ class Solution:
                 bitsInWindow[int(s[l])] -= 1
                 l += 1
 
-            substringCount += r - l + 1            
-            # print('l', l, 'r', r)
-            # print('bitsInWindow', bitsInWindow)
-            # print('substringCount', substringCount)
+            substringCount += r - l + 1
         
         return substringCount
