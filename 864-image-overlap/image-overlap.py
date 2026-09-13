be277129cd2236ed2b0d4b7725f9c8373a0b77dd
Brok3nPix3l <rjbabala@yahoo.com>
2026-09-12 21:24:26 -0400

    Time: 239 ms (61.64%) | Memory: 19.8 MB (28.84%) - LeetSync

--- 864-image-overlap/image-overlap.py
+++ 864-image-overlap/image-overlap.py
@@ -24,12 +24,12 @@ class Solution:
                 if translations_1_to_2[delta] > largest_overlap_count:
                     largest_overlap_count = translations_1_to_2[delta]
         
-        translations_2_to_1 = defaultdict(int)
-        for x1, y1 in img2_one_positions:
-            for x2, y2 in img1_one_positions:
-                delta = (x2 - x1, y2 - y1)
-                translations_2_to_1[delta] += 1
-                if translations_2_to_1[delta] > largest_overlap_count:
-                    largest_overlap_count = translations_2_to_1[delta]
+        # translations_2_to_1 = defaultdict(int)
+        # for x1, y1 in img2_one_positions:
+        #     for x2, y2 in img1_one_positions:
+        #         delta = (x2 - x1, y2 - y1)
+        #         translations_2_to_1[delta] += 1
+        #         if translations_2_to_1[delta] > largest_overlap_count:
+        #             largest_overlap_count = translations_2_to_1[delta]
         
         return largest_overlap_count
