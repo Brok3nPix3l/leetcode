f5eefcff89c2ee85a6ae9dcda72a9e9a6816cb9c
Brok3nPix3l <rjbabala@yahoo.com>
2026-09-20 14:51:19 -0400

    Time: 3 ms (66.99%) | Memory: 19.9 MB (10.26%) - LeetSync

---
+++ 3736-find-valid-pair-of-adjacent-digits-in-string/find-valid-pair-of-adjacent-digits-in-string.py
@@ -0,0 +1,21 @@
+from collections import Counter
+import functools
+
+class Solution:
+    def findValidPair(self, s: str) -> str:
+        self.c = Counter(s)
+
+        for i in range(len(s) - 1):
+            if s[i] == s[i + 1]:
+                continue
+            
+            if self.appearsCorrectNumberOfTimes(s[i]) and self.appearsCorrectNumberOfTimes(s[i + 1]):
+                return s[i] + s[i + 1]
+        
+        return ""
+
+    @cache
+    def appearsCorrectNumberOfTimes(self, d: chr) -> bool:
+        num = int(d)
+
+        return self.c[d] == num
